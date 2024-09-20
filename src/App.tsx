9104906a3c483f1be0ed6cff64ece144c085a4a8
Mike7s <146244208+Mike7s@users.noreply.github.com>
2024-09-20 18:36:42 +0200

    update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,27 +13,31 @@ function App() {
   return (
     <>
       <div className="flex flex-col min-h-screen">
+        
         <NavBar />
 
-        <div className="relative flex-grow flex flex-col justify-center items-center space-y-4">
+        <div className="relative flex-grow flex flex-col justify-center items-center space-y-4 ">
           <h1
             className={` font-urbanist font-bold text-8xl sm:p-4 transition-all duration-700 transform ${
-              isVisible ? "opacity-100 translate-y-0" : "opacity-0 -translate-y-10"
+              isVisible
+                ? "opacity-100 translate-y-0"
+                : "opacity-0 -translate-y-10"
             }`}
           >
             Atletico San Lorenzo
           </h1>
           <p
             className={`font-urbanist text-center font-bold text-2xl transition-all duration-700 transform ${
-              isVisible ? "opacity-100 translate-y-0" : "opacity-0 -translate-y-10"
+              isVisible
+                ? "opacity-100 translate-y-0"
+                : "opacity-0 -translate-y-10"
             }`}
           >
             Benvenuti nella pagina ufficiale dell'Atletico San Lorenzo
           </p>
         </div>
 
-        {/* Rimuoviamo l'opacità dall'immagine */}
-        <div className="absolute inset-0 w-full h-full flex justify-center items-center pointer-events-none z-0">
+        <div className="absolute inset-0 w-full h-full flex justify-center items-center opacity-20 pointer-events-none z-[-1]">
           <img
             src="/SanLorenzo.jpeg"
             alt="Logo"
