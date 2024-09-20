c06d8c1590f0bc6f219d20e93dd9f9e458f60492
Mike7s <146244208+Mike7s@users.noreply.github.com>
2024-09-21 01:55:24 +0200

    update marker-icon

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,21 +1,35 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';  
+
+
+const customMarkerIcon = new L.Icon({
+  iconUrl: '/marker-icon.png', 
+  shadowUrl: '/marker-shadow.png',  
+  iconSize: [25, 41],  
+  iconAnchor: [12, 41],  
+  popupAnchor: [1, -34],  
+  shadowSize: [41, 41],  
+});
 
 const Map: React.FC = () => {
   return (
     <MapContainer
-      center={[40.931103, 14.183496]} 
+      center={[40.931103, 14.183496]}
       zoom={13}
-      style={{ height: '400px', width: '100%' }} 
+      style={{ height: '400px', width: '100%' }}
     >
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      <Marker position={[40.931103, 14.183496]}>
+      <Marker 
+        position={[40.931103, 14.183496]} 
+        icon={customMarkerIcon}  
+      >
         <Popup>
-          Indirizzo: <br /> Via innamorati 141, 80014 Giugliano in Campania (NA).
+          Indirizzo: <br /> Via Innamorati 141, 80014 Giugliano in Campania (NA).
         </Popup>
       </Marker>
     </MapContainer>
