1cddf51258cf10cca1d661f437882fb970483a11
Mike7s <146244208+Mike7s@users.noreply.github.com>
2024-09-21 02:01:38 +0200

    update img presidente

--- src/pages/chiSiamo.tsx
+++ src/pages/chiSiamo.tsx
@@ -3,33 +3,30 @@ import Footer from "../components/footer";
 import PalmaresList from "../components/palmares";
 import { useState } from "react";
 
-
-
 function Info() {
-
-  const [loading,setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   return (
     <>
       <NavBar />
       <main className="bg-sanlorenzoBlu text-white">
-        <div className=" pb-16 ">
-          {!loading && 
-          <div className="animate-pulse h-screen">LOADING</div>
-          }
-          
+        <div className="pb-16">
+          {!loading && <div className="animate-pulse h-screen">LOADING</div>}
+
           <img
-            onLoad={ ()=> setLoading(true)}
+            onLoad={() => setLoading(true)}
             src="/IlPresidente.jpeg"
             alt="Logo San Lorenzo"
-            className={` ${loading ? '' : 'hidden'} w-full pb-5 min-h-screen `}
+            className={`${
+              loading ? "" : "hidden"
+            } w-full h-auto lg:h-screen `} 
           />
-        <h1 className="flex justify-center font-urbanist text-6xl px-5 pb-10 pt-20 font-bold sm:pl-10">
-          La nostra storia 
-        </h1>
 
+          <h1 className="flex justify-center font-urbanist text-6xl px-5 pb-10 pt-20 font-bold sm:pl-10">
+            La nostra storia
+          </h1>
 
-          <p className="font-urbanist  pl-5 pr-6 ">
+          <p className="font-urbanist pl-5 pr-6">
             L'Atletico San Lorenzo è una società calcistica amatoriale fondata
             il 31 agosto 2015 da Valerio Cocorullo. Questa iniziativa nasce
             dall'amore per il calcio e dalla volontà di creare un ambiente in
